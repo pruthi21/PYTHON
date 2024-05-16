88180556fe43d14ad340fe72dbac79d297571f72
pruthi21 <pruthikaw@gmail.com>
2024-05-16 17:36:41 +0530

    'conditional'

---
+++ conditional.py
@@ -0,0 +1,16 @@
+if 1 == 1:
+    print(True)
+else:
+    print(False)
+
+
+
+a=19
+b=1
+c=0
+if a>b and a>c:
+    print(a, "is greater")
+elif b>a and b>c:
+    print(b, "is greater")
+else:
+    print("")
