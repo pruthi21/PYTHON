aba3d53ee84a5a497e877adac7939723881e18c7
pruthi21 <pruthikaw@gmail.com>
2024-07-22 20:43:56 +0530

    'dic'

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+my_dict ={'name': 'pruthi','age':20, 'city': 'mumbai'}
+print(my_dict)
+
+user_dict ={}
+num = int(input('enter the number of entry:'))
+for i in range(num):
+    key= input('enter key: ')
+    value = input('enter value: ')
+
+    dictionary.update({key : value})
+print("the dictionary is ",user_dict)
