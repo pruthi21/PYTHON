ce79410c20ebde6b83f4269f4b004bde5cd122fe
pruthi21 <pruthikaw@gmail.com>
2024-07-30 20:51:02 +0530

    'bank'

---
+++ bank_app.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class intrestcal(ABC):
+    @abstractmethod
+    def cal_simple_intrest(self,rate,time, rule):
+        pass
+    @abstractmethod
+    def total_amount(self, rule, intrest):
+        pass
+
+class simpleintrest(intrestcal):
+    
